f72b86e23636093e292e92d547a3ee09344b4d9b
Tab-ex <52351044+Tab-ex@users.noreply.github.com>
2021-03-01 09:06:53 +0300

    Update Baymuhanov week 1 ex 1.cpp

--- Baymuhanov week 1 ex 1.cpp
+++ Baymuhanov week 1 ex 1.cpp
@@ -1,15 +1,21 @@
 ﻿
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-    cout <<"**     **\n"
+    ofstream fout("1 weak 1 ex.txt");
+
+    fout <<"**     **\n"
         << "**     **\n"
         << "*********\n"
         << "*********\n"
         << "**     **\n"
         << "**     **\n";
+    fout.close();
+    cout << ("Check the file: 1 weak 1 ex.txt")
+    return 0;
 }
 
 
