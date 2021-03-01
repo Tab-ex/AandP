18fcc8cd30f4dd0fedb17bb9676a9c2dea52e3a6
Tab-ex <52351044+Tab-ex@users.noreply.github.com>
2021-03-01 12:55:03 +0300

    Update Baymuhanov week 1 ex 3.cpp

--- Baymuhanov week 1 ex 3.cpp
+++ Baymuhanov week 1 ex 3.cpp
@@ -1,14 +1,16 @@
 ﻿#include <iostream>
-
+#include <fstream>
 int main()
 {
     system("color F3");
+    std::ofstream f("1 weak 3 ex.txt");
     std::cout << "Lead your three numbers\n";
     float Num1, Num2, Num3;
 
     std::cin >> Num1 >> Num2>> Num3;
-    std::cout << "average of numbers: " << (Num1 + Num2 + Num3) / 3 << "\n";
-    std::cout << "difference of the doubled sum of the first and third numbers and tripled the second  numbers:"<<((Num1+Num3)*2)-(Num2*3) << "\n";
+    f         << "Lead your three numbers\t" << Num1 << "\t" << Num2 << "\t" << Num3 << std::endl
+              << "average of numbers: " << (Num1 + Num2 + Num3) / 3 << "\n"
+              << "difference of the doubled sum of the first and third numbers and tripled the second  numbers:"<<((Num1+Num3)*2)-(Num2*3) << "\n";
       
-
+    f.close();
 }
