bc69cadb769acc8d38c0263dd2f9cb63d2a1d632
Tab-ex <52351044+Tab-ex@users.noreply.github.com>
2021-03-13 19:38:42 +0300

    Create Baymuhanov week 5 ex 1.cpp

---
+++ Baymuhanov week 5 ex 1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+
+
+using namespace std;
+
+
+int main()
+{
+	float x, y;
+	cin >> x ;
+	if (x == 0) 
+	{
+		y = -2;
+		cout << y;
+	}
+	else if (x < 0)
+	{
+		x *= -1;
+		y =2* (x+4) -6;
+		cout << y;
+	}
+	else 
+	{
+		y = x * 1.5 + 5;
+		cout << y;
+	}
+}
