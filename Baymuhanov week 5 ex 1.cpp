bbc7da5ff1c1c3f2b22d1f7e08872f855a1676cb
Tab-ex <52351044+Tab-ex@users.noreply.github.com>
2021-03-15 11:05:54 +0300

    Update Baymuhanov week 5 ex 1.cpp

--- Baymuhanov week 5 ex 1.cpp
+++ Baymuhanov week 5 ex 1.cpp
@@ -8,21 +8,28 @@ using namespace std;
 int main()
 {
 	float x, y;
+	ofstream fout("Ex 1.txt");
+	cout << "Enter the number ";
+	fout << "Enter the number ";
+	fout << x << endl;
 	cin >> x ;
 	if (x == 0) 
 	{
 		y = -2;
 		cout << y;
+		fout << y << endl;
 	}
 	else if (x < 0)
 	{
 		x *= -1;
 		y =2* (x+4) -6;
 		cout << y;
+		fout << y << endl;
 	}
 	else 
 	{
 		y = x * 1.5 + 5;
 		cout << y;
+		fout << y << endl;
 	}
 }
