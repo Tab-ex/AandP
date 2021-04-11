51ebf8cd780de6737e14747d444b3c5dee9d3cda
Tab-ex <52351044+Tab-ex@users.noreply.github.com>
2021-04-11 21:13:56 +0300

    Update Baymuhanov week 1 ex 3.cpp

--- week 1/Baymuhanov week 1 ex 3.cpp
+++ week 1/Baymuhanov week 1 ex 3.cpp
@@ -1,14 +1,15 @@
 ﻿#include <iostream>
 
+void F(int Num1,int Num2, int Num2)
+{
+     std::cout << "average of numbers: " << (Num1 + Num2 + Num3) / 3 << "\n";
+    std::cout << "difference of the doubled sum of the first and third numbers and tripled the second  numbers:"<<((Num1+Num3)*2)-(Num2*3) << "\n";
+}
+
 int main()
 {
     system("color F3");
     std::cout << "Lead your three numbers\n";
     float Num1, Num2, Num3;
-
-    std::cin >> Num1 >> Num2>> Num3;
-    std::cout << "average of numbers: " << (Num1 + Num2 + Num3) / 3 << "\n";
-    std::cout << "difference of the doubled sum of the first and third numbers and tripled the second  numbers:"<<((Num1+Num3)*2)-(Num2*3) << "\n";
-      
 
 }
