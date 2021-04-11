e4004e83c7cd8919abc55f72bf03303ea4091845
Tab-ex <52351044+Tab-ex@users.noreply.github.com>
2021-04-11 21:10:54 +0300

    Update Baymuhanov week 1 ex 2.cpp

--- week 1/Baymuhanov week 1 ex 2.cpp
+++ week 1/Baymuhanov week 1 ex 2.cpp
@@ -1,6 +1,13 @@
 ﻿#include <iostream>
 using namespace std;
 
+void Mat(float Length, float width )
+{
+    
+    cout << "|square \t| "   <<   "\t|\n"
+        << "|square area\t| " << Length * width<<"\t|\n"
+        << "|perimeter\t| " << (Length + width)*2 << "\t|\n";
+}
 
 int main()
 { 
@@ -8,9 +15,8 @@ int main()
     float Length, width;
     cout << "Length and width of the square : ";
     cin >> Length >> width;
-    cout << "|square \t| "   <<   "\t|\n"
-        << "|square area\t| " << Length * width<<"\t|\n"
-        << "|perimeter\t| " << (Length + width)*2 << "\t|\n";
+    Mat(Length,width);
+    
 
 
 }
