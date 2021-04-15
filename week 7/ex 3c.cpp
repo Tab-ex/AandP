13716bdc928a6749ca5a29be14ebbfd91208c237
Tab-ex <52351044+Tab-ex@users.noreply.github.com>
2021-04-15 09:11:53 +0300

    Update ex 3c.cpp

--- week 7/ex 3c.cpp
+++ week 7/ex 3c.cpp
@@ -4,18 +4,23 @@ using namespace std;
 
 int  k, x;
 float y=0;
-
+void F(int k1,int x1)
+{
+	for (int i = 2; i <k; i+=2)
+	{
+		y = y + 1.0/ (i * x);
+	}
+	cout << y;
+}
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	cout << ("Ведите значения K и X \n");
 	cin >> k>>x;
-	for (int i = 2; i <k; i+=2)
-	{
-		y = y + 1.0/ (i * x);
-	}
-	cout << y;
+	F(k,x);
+	return 0;
+	
 }
 
 
